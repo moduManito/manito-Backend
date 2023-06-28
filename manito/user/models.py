bca31a0763baeadc2a73de7bfa0dadace7fda677
koomchang <leahpar0401@gmail.com>
2023-06-28 21:04:57 +0900

    feat: user model

--- manito/user/models.py
+++ manito/user/models.py
@@ -1,1 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+
+class User(AbstractBaseUser):
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    email = models.CharField(
+        verbose_name="이메일",
+        max_length=255,
+    )
+
+    name = models.CharField(
+        verbose_name="이름",
+        max_length=255,
+    )
+
+    is_kakao = models.BooleanField(
+        default=False
+    )
