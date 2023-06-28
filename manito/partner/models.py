b8da4b6248077ab483d2bbc55bd0ea99c47e9adc
koomchang <leahpar0401@gmail.com>
2023-06-28 21:18:27 +0900

    feat: partner model

--- manito/partner/models.py
+++ manito/partner/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Partner(models.Model):
+    class Meta:
+        db_table = 'partner'
+        verbose_name = 'Partner'
+
+    sender = models.ForeignKey(
+        'user.User',
+        verbose_name="보내는 사람",
+        on_delete=models.CASCADE
+    )
+
+    receiver = models.ForeignKey(
+        'user.User',
+        verbose_name="받는 사람",
+        on_delete=models.CASCADE
+    )
