68d7b09780485b32d324dbf31dcd71cac62f1c42
koomchang <leahpar0401@gmail.com>
2023-06-30 00:21:56 +0900

    fix: changed parter data

--- manito/partner/serializers/partnerSerializer.py
+++ manito/partner/serializers/partnerSerializer.py
@@ -1,11 +1,9 @@
-from partner.models import Partner
 from rest_framework import serializers
-from manito.serializers import AbstractManitoSerializer
+
+from partner.models import Partner
 
 
 class PartnerSerializer(serializers.ModelSerializer):
-    manito = AbstractManitoSerializer(read_only=True)
-
     class Meta:
         model = Partner
         fields = [
